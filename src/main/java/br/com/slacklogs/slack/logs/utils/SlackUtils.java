397c6b38ea96fbe490418a6016e3b9fb6d8956d7
Matheus Fernandes <matheus.santos@datahub.com.br>
2021-06-04 17:38:12 -0300

    Post slack messages

--- src/main/java/br/com/slacklogs/slack/logs/utils/SlackUtils.java
+++ src/main/java/br/com/slacklogs/slack/logs/utils/SlackUtils.java
@@ -1,36 +1,44 @@
 package br.com.slacklogs.slack.logs.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.slacklogs.slack.logs.dto.SlackMessage;
 
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
-import java.io.IOException;
-
 public class SlackUtils {
-	private static String slackWebhookUrl = "https://hooks.slack.com/services/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";
+
+	private static final String URL = "https://slack.com/api/chat.postMessage";
+	private static final String TOKEN = "xoxb-2144825941524-2162482801536-TpgnpwuODF1fdsLsYeRyo956";
+	private static final String CHANNEL = "C023VS2M6BG";
 
 	public static void sendMessage(SlackMessage message) {
-		CloseableHttpClient client = HttpClients.createDefault();
-		HttpPost httpPost = new HttpPost(slackWebhookUrl);
+		RestTemplate template = new RestTemplate();
 
-		try {
-			ObjectMapper objectMapper = new ObjectMapper();
-			String json = objectMapper.writeValueAsString(message);
+		HttpHeaders headers = createHeaders();
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL);
+		HashMap<String, String> body = setBody(message);
+		HttpEntity<?> entity = new HttpEntity<>(body, headers);
 
-			StringEntity entity = new StringEntity(json);
-			httpPost.setEntity(entity);
-			httpPost.setHeader("Accept", "application/json");
-			httpPost.setHeader("Content-type", "application/json");
+		template.exchange(builder.toUriString(), HttpMethod.POST, entity, Void.class);
+	}
 
-			client.execute(httpPost);
-			client.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static HashMap<String, String> setBody(SlackMessage message) {
+		HashMap<String, String> body = new HashMap<>();
+		body.put("channel", CHANNEL);
+		body.put("text", message.getText());
+		return body;
+	}
+
+	private static HttpHeaders createHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + TOKEN);
+		headers.add("Accept", "*/*");
+		headers.add("Content-type", "application/json");
+		return headers;
 	}
 }
