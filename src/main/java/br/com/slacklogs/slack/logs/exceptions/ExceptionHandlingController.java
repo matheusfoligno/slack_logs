e84938b3d3c63e3be894d5aa4cf1a0a7e57a15ac
Matheus Fernandes <matheus.santos@datahub.com.br>
2021-06-06 22:19:53 -0300

    Kafka docker

--- src/main/java/br/com/slacklogs/slack/logs/exceptions/ExceptionHandlingController.java
+++ src/main/java/br/com/slacklogs/slack/logs/exceptions/ExceptionHandlingController.java
@@ -2,28 +2,30 @@ package br.com.slacklogs.slack.logs.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import br.com.slacklogs.slack.logs.dto.SlackMessage;
-import br.com.slacklogs.slack.logs.utils.SlackUtils;
+import br.com.slacklogs.slack.logs.service.KafkaProducerService;
 
 @ControllerAdvice
 public class ExceptionHandlingController {
 
+	@Autowired
+	private KafkaProducerService kafkaProducer;
+
 	@ExceptionHandler(BadRequestException.class)
 	public void handleErrorBadRequest(HttpServletRequest req, BadRequestException ex) {
-		sendSlack(HttpStatus.BAD_REQUEST + " - " + ex.getMessage());
+		sendKafkaProducer(HttpStatus.BAD_REQUEST + " - " + ex.getMessage());
 	}
 
 	@ExceptionHandler(NotFoundException.class)
 	public void handleErrorNotFoundException(HttpServletRequest req, NotFoundException ex) {
 	}
 
-	private void sendSlack(String message) {
-		SlackMessage slackMessage = new SlackMessage(message);
-		SlackUtils.sendMessage(slackMessage);
+	private void sendKafkaProducer(String message) {
+		kafkaProducer.sendMessage(message);
 	}
 
 }
---
+++ src/main/java/br/com/slacklogs/slack/logs/service/KafkaProducerService.java
@@ -0,0 +1,19 @@
+package br.com.slacklogs.slack.logs.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+	private static final String TOPIC = "slack-messages";
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public void sendMessage(String message) {
+		this.kafkaTemplate.send(TOPIC, message);
+	}
+
+}
